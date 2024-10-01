8bcf57a19b0a2f56b1f853473587d85864da7d8d
lb5014 <lb5014@naver.com>
2024-10-01 10:17:01 +0900

    사이트 정리

---
+++ 배경색만들기/js/script.js
@@ -0,0 +1,17 @@
+const text = document.querySelector('#colorText');
+const color = document.querySelector('#colorPicker');
+
+const colorBg = () =>{
+    document.body.style.backgroundColor=color.value;
+
+    if(color.value === '#ffffff'){
+        text.textContent =`컬러 코드 :${color.value}(white)`;
+    }
+    else if(color.value === '#000000'){
+        text.textContent=`컬러 코드 : ${color.value}(black)`;
+    }
+    else {
+        text.textContent=`컬러 코드 : ${color.value}`;
+    }
+}
+color.addEventListener('input',colorBg);
